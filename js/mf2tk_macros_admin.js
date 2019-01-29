188e886c3bf6cee41df6ae1fc52ff6b77e2741e4
Magenta Cuda <magenta.cuda@yahoo.com>
2019-01-29 08:05:13 -1000

    add support for Gutenberg

--- js/mf2tk_macros_admin.js
+++ js/mf2tk_macros_admin.js
@@ -16,17 +16,28 @@ jQuery(document).ready(function(){
     divShortcode.find("button#mf2tk-shortcode-tester-evaluate,button#mf2tk-shortcode-tester-evaluate-and-prettify").click(function(){
         var permalink=jQuery("#sample-permalink a");
         if(!permalink.length){
-            window.alert("Error: Permalink not found. Please report this to the developer.");
-            return;
+            // Gutenberg
+            permalink=jQuery("#wp-admin-bar-view a");
+            if(!permalink.length){
+                window.alert("Error: Permalink not found. Please report this to the developer.");
+                return;
+            }
         }
         var url=permalink[0].href+"?mc-sct=tpcti_eval_post_content"
         var prettify=this.id==="mf2tk-shortcode-tester-evaluate-and-prettify";
         var post_id=jQuery("form#post input#post_ID[type='hidden']").val();
         var source=jQuery("div#mf2tk-shortcode-tester div#mf2tk-shortcode-tester-area-source textarea").val();
-        var button=jQuery("button#sct_ix-shortcode-tester")[0];
+        var button=jQuery("button#sct_ix-shortcode-tester");
+        var nonce;
+        if(button.length){
+            nonce=button[0].dataset.nonce;
+        }else{
+            // Gutenberg
+            nonce=mf2tk_macros_admin.shortcode_tester_nonce;
+        }
         jQuery("div#mf2tk-shortcode-tester div#mf2tk-shortcode-tester-area-result textarea").val("Evaluating..., please wait...");
         // Use AJAX to request the server to evaluate the post content fragment
-        jQuery.post(url,{action:'tpcti_eval_post_content',post_id:post_id,post_content:source,prettify:prettify,nonce:button.dataset.nonce},function(r){
+        jQuery.post(url,{action:'tpcti_eval_post_content',post_id:post_id,post_content:source,prettify:prettify,nonce:nonce},function(r){
             jQuery("div#mf2tk-shortcode-tester div#mf2tk-shortcode-tester-area-result textarea").val(r);
         });
     });
@@ -60,4 +71,17 @@ jQuery(document).ready(function(){
         divPopupOuter.show();
         divShortcode.show();
     });
+    // Gutenberg
+    window.setTimeout(function(){
+        jQuery(".edit-post-header-toolbar").append('<button type="button" aria-label="Redo" aria-disabled="true" class="components-button components-icon-button sct_ix-shortcode-tester"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20"><path d="M8.58 2.39c.32 0 .59.05.81.14 1.25.55 1.69 2.24 1.7 3.97.59-.82 2.15-2.29 3.41-2.29s2.94.73 3.53 3.55c-1.13-.65-2.42-.94-3.65-.94-1.26 0-2.45.32-3.29.89.4-.11.86-.16 1.33-.16 1.39 0 2.9.45 3.4 1.31.68 1.16.47 3.38-.76 4.14-.14-2.1-1.69-4.12-3.47-4.12-.44 0-.88.12-1.33.38C8 10.62 7 14.56 7 19H2c0-5.53 4.21-9.65 7.68-10.79-.56-.09-1.17-.15-1.82-.15C6.1 8.06 4.05 8.5 2 10c.76-2.96 2.78-4.1 4.69-4.1 1.25 0 2.45.5 3.2 1.29-.66-2.24-2.49-2.86-4.08-2.86-.8 0-1.55.16-2.05.35.91-1.29 3.31-2.29 4.82-2.29zM13 11.5c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5.67 1.5 1.5 1.5 1.5-.67 1.5-1.5z"/></svg></button>')
+            .find(".sct_ix-shortcode-tester").click(function(){
+            divShortcode.find("button#mf2tk-shortcode-tester-show-both").prop("disabled",true);
+            divShortcode.find("button#mf2tk-shortcode-tester-show-source,button#mf2tk-shortcode-tester-show-result").prop("disabled",false);
+            divShortcode.find("div#mf2tk-shortcode-tester-area-source textarea").val("");
+            divShortcode.find("div#mf2tk-shortcode-tester-area-result textarea").val("");
+            divShortcode.find("div.sct_ix-shortcode_tester_half").removeClass("sct_ix-this_half_only").show();
+            divPopupOuter.show();
+            divShortcode.show();
+        });
+    },1000);
 });
