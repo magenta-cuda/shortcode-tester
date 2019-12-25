13063d4e54eed2ee2e190a151b5596eaba2ade27
Magenta Cuda <magenta.cuda@yahoo.com>
2019-12-25 08:16:03 -1000

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp  = require('gulp');
+var chmod = require('gulp-chmod');
+
+gulp.task('dev', function(){
+    return gulp.src(['*.php', '*.ini', '*.txt', 'css/*.css', 'js/*.js', '!gulpfile.js'], {"base":"."})
+        .pipe(chmod(0644))
+        .pipe(gulp.dest('/var/www/html/wp-content/plugins/shortcode-tester'))
+});
+
