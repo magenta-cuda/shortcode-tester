99acfe7f497c18abe344815509cceebe8f664ec6
Magenta Cuda <magenta.cuda@yahoo.com>
2019-12-25 12:50:21 -1000

    add prod to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,9 @@ gulp.task('dev', function(){
         .pipe(gulp.dest('/var/www/html/wp-content/plugins/shortcode-tester'))
 });
 
+gulp.task('prod', function(){
+    return gulp.src(['*.php', '*.ini', '*.txt', 'css/*.css', 'js/*.js', '!gulpfile.js', '!debug_utilities.php'], {"base":"."})
+        .pipe(chmod(0644))
+        .pipe(gulp.dest('/var/www/html/wp-content/plugins/shortcode-tester'))
+});
+
